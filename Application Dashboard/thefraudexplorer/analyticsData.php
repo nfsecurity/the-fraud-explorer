16813e8cd422a6c318eb392611b5acd9dad31bbb
NF <jrios@nfsec.co>
2020-04-05 15:43:42 -0500

    Improved cached CSS

--- Application Dashboard/thefraudexplorer/analyticsData.php
+++ Application Dashboard/thefraudexplorer/analyticsData.php
@@ -57,7 +57,7 @@ include "lbs/elasticsearch.php";
         <!-- Charts CSS -->
 
         <link rel="stylesheet" type="text/css" href="css/analyticsData.css?<?php echo filemtime('css/analyticsData.css') ?>"/>
-        <link rel="stylesheet" type="text/css" href="css/chartAnalytics.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="css/chartAnalytics.css?<?php echo filemtime('css/chartAnalytics.css') ?>" media="screen" />
 
         <!-- Load Chart.js -->
 
